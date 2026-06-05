46ac9f76d5730ea9ced958f644f5978103be9a9a
Eph23 <ephraims.818@gmail.com>
2026-06-05 15:29:10 +0600

    JS--COMPLETE-JS--REV -- FUNDAMENTALS -- 08. Introduction to Objects

---
+++ 01. Fundamentals/08. Introduction to Objects/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// ANCHOR: Object literal
+/* NOTE: 
+An object is a standalone data structure that stores related data as a collection of key-value pairs, where each key is linked to a specific value
+*/
+const eph = {
+    firstName: "Ephraim",
+    lastName: "S",
+    age: 2037 - 1988,
+    job: "Web Dev",
+    friends: ["Michael", "Peter", "Bob"],
+};
+
+console.log(eph);
