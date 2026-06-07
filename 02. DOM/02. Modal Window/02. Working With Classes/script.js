f7b1f1a71e2bc01123096da088c4828a846cb0b2
Eph23 <ephraims.818@gmail.com>
2026-06-08 01:48:39 +0600

    JS--COMPLETE-JS--REV -- DOM -- MODAL-WINDOW -- 02. Working With Classes

--- 02. DOM/02. Modal Window/02. Working With Classes/script.js
+++ 02. DOM/02. Modal Window/02. Working With Classes/script.js
@@ -3,10 +3,16 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-const closeModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 btnsOpenModal.forEach(btn => {
   btn.addEventListener('click', function () {
-    console.log('clicked', btn.textContent);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 });
