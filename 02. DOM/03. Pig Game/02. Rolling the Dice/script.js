406e484c67f98bb3ea578477a182760e7981194c
Eph23 <ephraims.818@gmail.com>
2026-06-09 04:09:40 +0600

    JS--COMPLETE-JS--REV -- DOM -- PIG-GAME -- 02. Rolling the Dice

---
+++ 02. DOM/03. Pig Game/02. Rolling the Dice/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+const score = [0, 0];
+
+btnRoll.addEventListener('click', function () {
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    current0El.textContent = currentScore; // TODO: Change later
+  } else {
+  }
+});
