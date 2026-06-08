98094636869c394ecd5cc871da7b22312a9fa577
Eph23 <ephraims.818@gmail.com>
2026-06-09 03:52:58 +0600

    JS--COMPLETE-JS--REV -- DOM -- PIG-GAME -- 01. Selecting DOM Elements

---
+++ 02. DOM/03. Pig Game/01. Selecting DOM Elements/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add('hidden');
