4d3ecc64d2649b52f4d4aa78d05e782e5542c2a1
Eph23 <ephraims.818@gmail.com>
2026-06-11 16:53:04 +0600

    JS--COMPLETE-JS--REV -- DATA-STRUCTURES -- 02. Destructuring Objects

---
+++ 04. Data Structures/02. Destructuring Objects/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+};
+
+// ANCHOR: Destructuring objects
+/* NOTE: 
+a shorthand syntax introduced in ES6 that allows you to unpack values from objects directly into distinct variables
+*/
+const { name, categories, openingHours } = restaurant;
+console.log(name);
+console.log(categories);
+console.log(openingHours);
+
+// ANCHOR: Custom variable name
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName);
+console.log(hours);
+console.log(categories);
+
+// ANCHOR: Default value
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu);
+console.log(starters);
+
+// ANCHOR: Mutating variables
+let a = 111;
+let b = 999;
+console.log(a, b);
+
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+
+console.log(o, c);
+
+// ANCHOR: Destructuring object method arguments
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 2,
+});
