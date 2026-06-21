3c274dd0f6e91c5136cc5007fd52d90d5c91c798
Eph23 <ephraims.818@gmail.com>
2026-06-21 17:57:17 +0600

    JS--COMPLETE-JS--REV -- DATA-STRUCTURES -- 12. Sets

---
+++ 04. Data Structures/12. Sets/script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const openingHours = {
+  wed: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  openingHours,
+
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+// ANCHOR: Sets
+/* NOTE: 
+A JavaScript Set is a built-in object that stores a collection of unique values of any type. Unlike arrays, a Set cannot hold duplicate values; any duplicate addition is automatically ignored.
+*/
+const orderSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(orderSet);
+
+console.log(new Set('Ephraim'));
+
+// ANCHOR: Set methods
+console.log(orderSet.size);
+
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+console.log(orderSet);
+console.log(orderSet.size);
+
+orderSet.delete('Risotto');
+console.log(orderSet);
+
+// ANCHOR: Looping sets
+for (const order of orderSet) {
+  console.log(order);
+}
+
+// ANCHOR: Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
